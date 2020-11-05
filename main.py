96ffc3bd088cffffb8dcb701b945ad96acd1875b
weird.structure <sequence@myrealm.ca>
2020-11-05 16:43:50 -0700

    Merged code into one file, added requirements.txt.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from script import show_weather
+import requests
+
+def show_weather(locations, lang):
+  payload = {"":"nTqum", "lang": lang}
+  url = 'http://wttr.in/{place}'
+  locations = ['London', 'svo', 'Череповец']
+  for location in locations:
+    response = requests.get(url.format(place=location), params=payload)
+    response.raise_for_status()
+    print(response.text)
 
 locations = ['London', 'svo', 'Череповец']
 lang = "ru"
