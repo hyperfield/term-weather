9e4289acd57a461027d01da670dea65c6a0fe0bf
weird.structure <sequence@myrealm.ca>
2020-11-05 16:48:32 -0700

    Modified one variable (url -> url_template) in the script.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 
 def show_weather(locations, lang):
   payload = {"":"nTqum", "lang": lang}
-  url = 'http://wttr.in/{place}'
+  url_template = 'http://wttr.in/{place}'
   locations = ['London', 'svo', 'Череповец']
   for location in locations:
-    response = requests.get(url.format(place=location), params=payload)
+    response = requests.get(url_template.format(place=location), params=payload)
     response.raise_for_status()
     print(response.text)
 
